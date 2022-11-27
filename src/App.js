76674d54af21c3724f5a30783d21c1d43c2fe798
Patri Naskar <88491717+patri321@users.noreply.github.com>
2022-11-27 20:42:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ChatBot from 'react-simple-chatbot';
+import {Segment} from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const steps = [
+  {
+    id:'Greet',
+    message:'Hello,Welcome to our shop! How can we help you?',
+    trigger:'Ask name'
+  },
+  {
+    id:'Ask name',
+    message:'Enter your name',
+    trigger:'waiting'
+  },
+  {
+    id:'waiting',
+    user:true,
+    trigger:'name'
+  },
+  {
+    id:'name',
+    message:'Hi {previousValue}, Which category you are facing issues?',
+    trigger:'issues'
+  },
+  {
+    id:'issues',
+    options:[
+      {value:'Men',label:'Men',trigger:"Men"},
+      {value:'Women',label:'Women',trigger:"Women" },
+      {value:'Kids',label:'Kids',trigger:"Kids"},
+      {value:'Electronics Items',label:'Electronics Itemsds',trigger:"Electronics Items"},
+      {value:'Home Decor',label:'Home Decor',trigger:"Home Decor"},
+      {value:'Accesories',label:'Accesories',trigger:"Accesories"},
+    ]
+  },
+    {
+      id:'Men',
+      message:'Thanks for telling your issues.',
+      end:true
+
+    },
+    {
+      id:'Women',
+      message:'Thanks for telling your issues.',
+      end:true
+    },
+    {
+      id:'Kids',
+      message:'Thanks for telling your issues.',
+      end:true
+    },
+    {
+      id:'Electronics Items',
+      message:'Thanks for telling your issues.',
+      end:true
+    },
+    {
+      id:'Home Decor',
+      message:'Thanks for telling your issues.',
+      end:true
+   },
+  {
+     id:'Accesories',
+      message:'Thanks for telling your issues.',
+      end:true
 }
 
-export default App;
+    ];
+
+function App() {
+
+  
+    
+  return(
+  <>
+  <Segment floated="left">
+    <ChatBot steps={steps} />
+    </Segment>
+    
+    </>
+  );
+  }
+  export default App;
